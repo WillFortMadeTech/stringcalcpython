5f55982739a8ed2f1015958a09e3a022c1a0aecc
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-09-03 17:39:50 +0100

    Refactor: Retrun numbers and inline the add methoid in the tests

--- string_calculator.py
+++ string_calculator.py
@@ -1,7 +1,5 @@
 def add(numbers: str) -> str:
     if (numbers == ""):
         return "0"
-    elif (numbers =="2"):
-        return "2"
 
-    return "1"
+    return numbers
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -2,16 +2,10 @@ from string_calculator import add
 
 
 def test_add_with_empty_string_returns_zero():
-    result = add("")
-
-    assert result is "0"
+    assert add("") is "0"
 
 def test_add_with_1_returns_1():
-    result = add("1")
-
-    assert result is "1"
+    assert add("1") is "1"
 
 def test_add_with_2_returns_2():
-    result = add("2")
-    
-    assert result is "2"
+    assert add("2") is "2"
