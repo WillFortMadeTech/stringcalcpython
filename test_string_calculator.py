80cfae764327a53cf7751a055f91cbfa80315302
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-09-03 18:09:46 +0100

    Green: More passing tests

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -15,3 +15,9 @@ def test_add_with_2_and_5_returns_7():
 
 def test_add_with_2point2_and_5point2_returns_7point4():
     assert add("2.2,5.2") == "7.4"
+
+def test_add_with_1point1_and_2point2_returns_3point3():
+    assert add("1.1,2.2") == "3.3"
+
+def test_add_with_2point3_and_7point4_returns_7point7():
+    assert add("2.3,5.4") == "7.7"
