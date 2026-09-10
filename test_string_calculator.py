ee240530eb9ee690ddeb1c3c81e390badd088a3b
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-09-10 17:39:56 +0100

    Red: next example of adjacent separators

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -29,7 +29,8 @@ def test_can_add_newline_as_a_separator(input_str, expected):
 
 @pytest.mark.parametrize("input_str,expected",
     [
-        ["175.2,\n35","Number expected but '\n' found at position 6."]
+        ["175.2,\n35","Number expected but '\n' found at position 6."],
+        ["17\n,35","Number expected but '\n' found at position 4."]
     ]
 )
 def test_with_adjacent_separators(input_str, expected):
